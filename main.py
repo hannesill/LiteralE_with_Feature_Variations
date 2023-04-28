db9c06788371b96bd9a98a122f757d776e0e5494
hill <hill@techfak.uni-bielefeld.de>
2023-04-28 18:17:08 +0200

    Init project

---
+++ main.py
@@ -0,0 +1,305 @@
+import argparse
+import json
+from datetime import datetime
+import torch
+import os.path as osp
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+
+from dataset import LiteralLinkPredDataset
+from models import DistMult, DistMultLit
+
+
+def negative_sampling(edge_index, num_nodes, eta=1):
+    # Sample edges by corrupting either the subject or the object of each edge.
+    mask_1 = torch.rand(edge_index.size(0) * eta) < 0.5
+    mask_2 = ~mask_1
+
+    mask_1 = mask_1.to(DEVICE)
+    mask_2 = mask_2.to(DEVICE)
+
+    neg_edge_index = edge_index.clone().repeat(eta, 1)
+    neg_edge_index[mask_1, 0] = torch.randint(num_nodes, (1, mask_1.sum()), device=DEVICE)
+    neg_edge_index[mask_2, 1] = torch.randint(num_nodes, (1, mask_2.sum()), device=DEVICE)
+
+    return neg_edge_index
+
+
+def train_standard_lp(config,
+                      model_lp,
+                      loss_function_model,
+                      optimizer,
+                      dataset):
+    model_lp.train()
+    start = time.time()
+
+    train_edge_index_t = dataset.edge_index_train.t().to(DEVICE)
+    train_edge_type = dataset.edge_type_train.to(DEVICE)
+
+    edge_index_batches = torch.split(train_edge_index_t, config['batch_size'])
+    edge_type_batches = torch.split(train_edge_type, config['batch_size'])
+
+    batch_indices = np.arange(len(edge_index_batches))
+    np.random.shuffle(batch_indices)
+
+    loss_total = 0
+
+    for batch_index in tqdm(batch_indices):
+        edge_idxs, relation_idx = edge_index_batches[batch_index], edge_type_batches[batch_index]
+        optimizer.zero_grad()
+
+        edge_idxs_neg = negative_sampling(edge_idxs, dataset.num_entities, eta=config['eta'])
+
+        out_pos = model_lp.forward(edge_idxs[:, 0], relation_idx, edge_idxs[:, 1])
+        out_neg = model_lp.forward(edge_idxs_neg[:, 0], relation_idx.repeat(config['eta']), edge_idxs_neg[:, 1])
+
+        out = torch.cat([out_pos, out_neg], dim=0)
+        gt = torch.cat([torch.ones(len(relation_idx)), torch.zeros(len(relation_idx) * config['eta'])], dim=0).to(
+            DEVICE)
+
+        # print('size lp:', gt.size())
+
+        loss = loss_function_model(out, gt)
+
+        loss_total += loss.item()
+        loss.backward()
+        optimizer.step()
+
+    end = time.time()
+    print('elapsed time:', end - start)
+    print('loss:', loss_total / len(edge_index_batches))
+
+
+@torch.no_grad()
+def compute_rank(ranks):
+    # fair ranking prediction as the average
+    # of optimistic and pessimistic ranking
+    true = ranks[0]
+    optimistic = (ranks > true).sum() + 1
+    pessimistic = (ranks >= true).sum()
+    return (optimistic + pessimistic).float() * 0.5
+
+
+@torch.no_grad()
+def compute_mrr_triple_scoring(model_lp, dataset, eval_edge_index, eval_edge_type,
+                               fast=False):
+    model_lp.eval()
+    ranks = []
+    num_samples = eval_edge_type.numel() if not fast else 5000
+    for triple_index in tqdm(range(num_samples)):
+        (src, dst), rel = eval_edge_index[:, triple_index], eval_edge_type[triple_index]
+
+        # Try all nodes as tails, but delete true triplets:
+        tail_mask = torch.ones(dataset.num_entities, dtype=torch.bool)
+        for (heads, tails), types in [
+            (dataset.edge_index_train, dataset.edge_type_train),
+            (dataset.edge_index_val, dataset.edge_type_val),
+            (dataset.edge_index_test, dataset.edge_type_test),
+        ]:
+            tail_mask[tails[(heads == src) & (types == rel)]] = False
+
+        tail = torch.arange(dataset.num_entities)[tail_mask]
+        tail = torch.cat([torch.tensor([dst]), tail])
+        head = torch.full_like(tail, fill_value=src)
+        eval_edge_typ_tensor = torch.full_like(tail, fill_value=rel).to(DEVICE)
+
+        out = model_lp.forward(head.to(DEVICE), eval_edge_typ_tensor.to(DEVICE), tail.to(DEVICE))
+
+        rank = compute_rank(out)
+        ranks.append(rank)
+
+        # Try all nodes as heads, but delete true triplets:
+        head_mask = torch.ones(dataset.num_entities, dtype=torch.bool)
+        for (heads, tails), types in [
+            (dataset.edge_index_train, dataset.edge_type_train),
+            (dataset.edge_index_val, dataset.edge_type_val),
+            (dataset.edge_index_test, dataset.edge_type_test),
+        ]:
+            head_mask[heads[(tails == dst) & (types == rel)]] = False
+
+        head = torch.arange(dataset.num_entities)[head_mask]
+        head = torch.cat([torch.tensor([src]), head])
+        tail = torch.full_like(head, fill_value=dst)
+        eval_edge_typ_tensor = torch.full_like(head, fill_value=rel).to(DEVICE)
+
+        out = model_lp.forward(head.to(DEVICE), eval_edge_typ_tensor.to(DEVICE), tail.to(DEVICE))
+
+        rank = compute_rank(out)
+        ranks.append(rank)
+
+    num_ranks = len(ranks)
+    ranks = torch.tensor(ranks, dtype=torch.float)
+    return (1. / ranks).mean().item(), \
+        ranks.mean().item(), \
+        (ranks[ranks <= 10].size(0) / num_ranks), \
+        (ranks[ranks <= 5].size(0) / num_ranks), \
+        (ranks[ranks <= 3].size(0) / num_ranks), \
+        (ranks[ranks <= 1].size(0) / num_ranks)
+
+
+def train_lp_objective(config, model_lp):
+    dataset = config['dataset']
+
+    loss_function_model = torch.nn.BCELoss(reduction='mean')
+    if config['alpha'] > 0:
+        optimizer = torch.optim.Adam(list(model_lp.parameters()), lr=config['lr'])
+    else:
+        optimizer = torch.optim.Adam(model_lp.parameters(), lr=config['lr'])
+
+    start_epoch = 1
+    model_lp.train()
+
+    # History of validation metrics
+    history = {"epoch": [],
+               "val_mrr": [],
+               "val_mr": [],
+               "val_hits10": [],
+               "val_hits5": [],
+               "val_hits3": [],
+               "val_hits1": []}
+
+    for epoch in range(start_epoch, 10):
+        print(f"--> Epoch {epoch}")
+        train_standard_lp(config,
+                          model_lp,
+                          loss_function_model,
+                          optimizer,
+                          dataset)
+        # Evaluating
+        if epoch % 5 == 0:
+            print("Evaluating model...")
+            mrr, mr, hits10, hits5, hits3, hits1 = compute_mrr_triple_scoring(model_lp,
+                                                                              dataset,
+                                                                              dataset.edge_index_val,
+                                                                              dataset.edge_type_val,
+                                                                              fast=True)
+            print('val mrr:', mrr, 'mr:', mr, 'hits@10:', hits10, 'hits@5:', hits5, 'hits@3:', hits3, 'hits@1:', hits1)
+
+            # Save validation metrics
+            history["epoch"].append(epoch)
+            history["val_mrr"].append(mrr)
+            history["val_mr"].append(mr)
+            history["val_hits10"].append(hits10)
+            history["val_hits5"].append(hits5)
+            history["val_hits3"].append(hits3)
+            history["val_hits1"].append(hits1)
+
+            # Save history dictionary
+            with open(f"results/{RUN_NAME}_history.json", "w+") as f:
+                json.dump(history, f)
+
+            # Save model
+            torch.save(model_lp.state_dict(), 'model_lp.pth')
+
+
+if __name__ == '__main__':
+    if torch.cuda.is_available():
+        print('Using CUDA')
+        DEVICE = torch.device('cuda')
+    else:
+        print('Using CPU')
+        DEVICE = torch.device('cpu')
+    RUN_NAME = 'feature_vs_objective_link_pred_' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    dataset_name = 'fb15k-237'
+    if not osp.isfile(f'data/{dataset_name}/processed.pt'):
+        print('Process dataset...')
+        dataset = LiteralLinkPredDataset(f'data/{dataset_name}')
+        torch.save(dataset, f'data/{dataset_name}/processed.pt')
+    print('Load processed dataset...')
+    dataset = torch.load(f'data/{dataset_name}/processed.pt')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lit", action="store_true")
+    args = parser.parse_args()
+    if args.lit:
+        model_type = "DistMultLit"
+    else:
+        model_type = "DistMult"
+    print(f"Model type: {model_type}")
+
+    # default config
+    config = {'dataset': dataset,
+              'dim': 200,
+              'lr': 0.001,
+              'batch_size': 256,
+              'dropout': 0.2,
+              'alpha': 0,
+              'eta': 5,
+              'reg': False,
+              'batch_norm': False}
+
+    # TODO: For DistMult+LiteralE config look at LiteralE paper
+    best_config_distmult = {
+        'dataset': dataset,
+        'dim': 100,
+        'lr': 0.00065,
+        'batch_size': 256,
+        'dropout': 0.2,
+        'alpha': 0,
+        'eta': 100,
+        'reg': False,
+        'batch_norm': False
+    }
+
+    # 14000, 1, 300 -> 14000, 300
+    dataset.features_txt = dataset.features_txt.squeeze()
+
+    # create model
+    if model_type == "DistMultLit":
+        model_lp = DistMultLit(dataset.num_entities,
+                               dataset.num_relations,
+                               dataset.features_num,
+                               dataset.features_txt,
+                               config['dim'])
+    else:
+        model_lp = DistMult(dataset.num_entities, dataset.num_relations, config['dim'])
+    model_lp.to(DEVICE)
+
+    print(dataset.num_entities, dataset.num_relations)
+    print(dataset.features_num.shape, dataset.features_txt.shape)
+
+    # train model
+    print("Start training...")
+    train_lp_objective(config, model_lp)
+
+    # test model
+    print("Start testing...")
+    mrr, mr, hits10, hits5, hits3, hits1 = compute_mrr_triple_scoring(model_lp,
+                                                                      dataset,
+                                                                      dataset.edge_index_test,
+                                                                      dataset.edge_type_test,
+                                                                      fast=True)
+    print('test mrr:', mrr, 'mr:', mr, 'hits@10:', hits10, 'hits@5:', hits5, 'hits@3:', hits3, 'hits@1:', hits1)
+
+    # Save test results
+    with open(f"results/{RUN_NAME}_test_results.txt", "w") as f:
+        f.write(f"test mrr: {mrr}, mr: {mr}, hits@10: {hits10}, hits@5: {hits5}, hits@3: {hits3}, hits@1: {hits1}")
+
+    # Load history dictionary
+    with open(f"results/{RUN_NAME}_history.json", "r") as f:
+        history = json.load(f)
+
+    # Get validation metrics histories
+    epochs = history["epoch"]
+    mrr_history = history["val_mrr"]
+    mr_history = history["val_mr"]
+    hits10_history = history["val_hits10"]
+    hits5_history = history["val_hits5"]
+    hits3_history = history["val_hits3"]
+    hits1_history = history["val_hits1"]
+
+    # Plot all histories and save figure
+    plt.plot(epochs, mrr_history, label="MRR")
+    plt.plot(epochs, mr_history, label="MR")
+    plt.plot(epochs, hits10_history, label="Hits@10")
+    plt.plot(epochs, hits5_history, label="Hits@5")
+    plt.plot(epochs, hits3_history, label="Hits@3")
+    plt.plot(epochs, hits1_history, label="Hits@1")
+    plt.xlabel("Epoch")
+    plt.ylabel("Validation metric")
+    plt.legend()
+    plt.savefig(f"results/{RUN_NAME}_history.png")
---
+++ models.py
@@ -0,0 +1,127 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.nn import Parameter
+from torch.nn.init import xavier_normal_, xavier_uniform_
+
+
+class DistMult(nn.Module):
+    def __init__(self, num_entities, num_relations, embedding_dim, dropout=0.2, batch_norm=False):
+        super().__init__()
+        self.batch_norm = batch_norm
+        self.entity_embedding = torch.nn.Embedding(num_entities, embedding_dim)
+        self.rel_embedding = torch.nn.Embedding(num_relations, embedding_dim)
+
+        self.dp_ent = torch.nn.Dropout(dropout)
+        self.dp_rel = torch.nn.Dropout(dropout)
+
+        self.bn_head = torch.nn.BatchNorm1d(embedding_dim)
+        self.bn_rel = torch.nn.BatchNorm1d(embedding_dim)
+        self.bn_tail = torch.nn.BatchNorm1d(embedding_dim)
+
+    def init(self):
+        xavier_normal_(self.entity_embedding.weight.data)
+        xavier_normal_(self.rel_embedding.weight.data)
+
+    def forward(self, head_idx, rel_idx, tail_idx):
+        h_head = self.dp_ent(self.entity_embedding(head_idx))
+        h_relation = self.dp_rel(self.rel_embedding(rel_idx))
+        h_tail = self.dp_ent(self.entity_embedding(tail_idx))
+
+        if self.batch_norm:
+            h_head = self.bn_head(h_head)
+            h_relation = self.bn_rel(h_relation)
+            h_tail = self.bn_tail(h_tail)
+
+        out = torch.sigmoid(torch.sum(h_head * h_relation * h_tail, dim=1))
+        out = torch.flatten(out)
+
+        return out
+
+
+##############################################################################################################
+# DistMultLit
+##############################################################################################################
+class Gate(nn.Module):
+
+    def __init__(self, num_numerical_literals, num_text_literals, embedding_dim):
+        super(Gate, self).__init__()
+        self.W_ze = nn.Linear(embedding_dim, embedding_dim)
+        self.W_zl_num = nn.Linear(num_numerical_literals, embedding_dim)
+        self.W_zl_text = nn.Linear(num_text_literals, embedding_dim)
+        self.bias = nn.Parameter(torch.zeros(embedding_dim))
+        self.W_h = nn.Linear(embedding_dim + num_numerical_literals + num_text_literals, embedding_dim)
+
+    def forward(self, entity_emb, entity_num_lit, entity_text_lit):
+        z_e = self.W_ze(entity_emb)
+        z_l_num = self.W_zl_num(entity_num_lit)
+        z_l_text = self.W_zl_text(entity_text_lit)
+        z = torch.sigmoid(z_e + z_l_num + z_l_text + self.bias)
+        h = torch.tanh(self.W_h(torch.cat((entity_emb, entity_num_lit, entity_text_lit), dim=1)))
+
+        return z * h + (1 - z) * entity_emb
+
+
+# TODO (wieder einbauen): Regularization (nicht analysieren, bring nix)
+# TODO (optional): early stopping
+class DistMultLit(nn.Module):
+
+    def __init__(self, num_entities, num_relations, numerical_literals, text_literals, embedding_dim,
+                 dropout=0.2, batch_norm=False):
+        super(DistMultLit, self).__init__()
+        # Initialize parameters
+        self.embedding_dim = embedding_dim
+        self.batch_norm = batch_norm
+        self.numerical_literals = numerical_literals
+        self.text_literals = text_literals
+        self.num_numerical_literals = numerical_literals.shape[1]
+        self.num_text_literals = text_literals.shape[1]
+
+        # Initialize embeddings
+        self.entity_embeddings = nn.Embedding(num_entities, embedding_dim)
+        self.relation_embeddings = nn.Embedding(num_relations, embedding_dim)
+        self.literal_embeddings = Gate(self.num_numerical_literals, self.num_text_literals, embedding_dim)
+
+        # Initialize loss function and weights
+        self.loss = nn.BCELoss()
+        self.init_weights()
+
+        # Initialize dropout and batch normalization
+        # TODO: Dropout auch für Gate?
+        self.dp_e = torch.nn.Dropout(dropout)
+        self.dp_r = torch.nn.Dropout(dropout)
+
+        # TODO: Batch normalization for both e1 and e2? Also for literal embeddings?
+        if self.batch_norm:
+            self.bn_e1 = torch.nn.BatchNorm1d(embedding_dim)
+            self.bn_r = torch.nn.BatchNorm1d(embedding_dim)
+            self.bn_e2 = torch.nn.BatchNorm1d(embedding_dim)
+
+    def init_weights(self):
+        nn.init.xavier_normal_(self.entity_embeddings.weight.data)
+        nn.init.xavier_normal_(self.relation_embeddings.weight.data)
+
+    def forward(self, e1_idx, r_idx, e2_idx):
+        # Get embeddings for entities and relations with dropout
+        e1_emb = self.dp_e(self.entity_embeddings(e1_idx))
+        r_emb = self.dp_r(self.relation_embeddings(r_idx))
+        e2_emb = self.dp_e(self.entity_embeddings(e2_idx))
+
+        # Batch normalization
+        if self.batch_norm:
+            e1_emb = self.bn_e1(e1_emb)
+            r_emb = self.bn_r(r_emb)
+            e2_emb = self.bn_e2(e2_emb)
+
+        # Get embeddings for numerical and text literals
+        # For e1
+        e1_num_lit = self.numerical_literals[e1_idx]
+        e1_text_lit = self.text_literals[e1_idx]
+        e1_emb = self.literal_embeddings(e1_emb, e1_num_lit, e1_text_lit)
+        # For e2
+        e2_num_lit = self.numerical_literals[e2_idx]
+        e2_text_lit = self.text_literals[e2_idx]
+        e2_emb = self.literal_embeddings(e2_emb, e2_num_lit, e2_text_lit)
+
+        result = torch.sum(e1_emb * r_emb * e2_emb, dim=1)
+        return torch.sigmoid(result)
