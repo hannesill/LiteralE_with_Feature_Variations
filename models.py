98f9acbb4af8a52509a40e1a189f9e490ecfd716
hill <hill@techfak.uni-bielefeld.de>
2023-06-18 01:02:43 +0200

    Refactor GateMulti

--- models.py
+++ models.py
@@ -137,24 +137,18 @@ class GateMulti(nn.Module):
     def __init__(self, emb_size, num_lit_size, txt_lit_size):
         super(GateMulti, self).__init__()
 
-        self.emb_size = emb_size
-        self.num_lit_size = num_lit_size
-        self.txt_lit_size = txt_lit_size
-
-        self.gate_activation = torch.sigmoid
-        self.g = nn.Linear(emb_size + num_lit_size + txt_lit_size, emb_size)
-
         self.gate_ent = nn.Linear(emb_size, emb_size, bias=False)
         self.gate_num_lit = nn.Linear(num_lit_size, emb_size, bias=False)
         self.gate_txt_lit = nn.Linear(txt_lit_size, emb_size, bias=False)
         self.gate_bias = nn.Parameter(torch.zeros(emb_size))
+        self.g = nn.Linear(emb_size + num_lit_size + txt_lit_size, emb_size)
 
     def forward(self, x_ent, x_lit_num, x_lit_txt):
-        x = torch.cat([x_ent, x_lit_num, x_lit_txt], 1)
-        g_embedded = torch.tanh(self.g(x))
-        gate = self.gate_activation(
+        z = torch.sigmoid(
             self.gate_ent(x_ent) + self.gate_num_lit(x_lit_num) + self.gate_txt_lit(x_lit_txt) + self.gate_bias)
-        output = (1 - gate) * x_ent + gate * g_embedded
+        h = torch.tanh(self.g(torch.cat([x_ent, x_lit_num, x_lit_txt], 1)))
+
+        output = (1 - z) * x_ent + z * h
 
         return output
 
