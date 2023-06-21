40025c82fdc288bd68f6e7f0b64f878d9fd6e429
hannesill <hill@techfak.uni-bielefeld.de>
2023-06-21 19:31:39 +0200

    New results

--- parameter.sh
+++ parameter.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-#SBATCH --job-name=DistMultModel
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=DistMultLit
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
 #SBATCH -p gpu
 #SBATCH -c 2
 #SBATCH --gres=gpu:1
-#SBATCH -w handy
-#SBATCH --time=02:00:00
+#SBATCH -w schlumpfine
+#SBATCH --time=08:00:00
 
 # Load the conda environment
 eval "$(~/miniconda3/bin/conda shell.bash hook)"
 conda activate LiteralE
 
-python main.py
+python3 main.py --lit
