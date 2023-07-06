7bc52f89be2e8a7c2bd3aa09c2374c4188ab77ef
hannesill <hill@techfak.uni-bielefeld.de>
2023-07-06 09:47:35 +0200

    New results

---
+++ param.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=linkpred
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+#SBATCH -p gpu
+#SBATCH -c 2
+#SBATCH --gres=gpu:1
+#SBATCH -w papaschlumpf
+#SBATCH --time=30:00:00
+
+# Load the conda environment
+eval "$(~/miniconda3/bin/conda shell.bash hook)"
+conda activate LiteralE
+
+python3 main.py --lit --filter 100 --attr --emb_dim 200
