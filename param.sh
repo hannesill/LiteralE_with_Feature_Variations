0b7f18e9e52c5332e18374ca3832e6f58e2eafde
hannesill <hill@techfak.uni-bielefeld.de>
2023-07-11 12:03:36 +0200

    New results

--- param.sh
+++ param.sh
@@ -8,11 +8,11 @@
 #SBATCH -p gpu
 #SBATCH -c 2
 #SBATCH --gres=gpu:1
-#SBATCH -w clumsy
-#SBATCH --time=60:00:00
+#SBATCH -w papaschlumpf
+#SBATCH --time=100:00:00
 
 # Load the conda environment
 eval "$(~/miniconda3/bin/conda shell.bash hook)"
 conda activate LiteralE
 
-python3 main.py --lit_mode txt --epochs 2000
+python3 main.py --lit_mode none  --epochs 4000
