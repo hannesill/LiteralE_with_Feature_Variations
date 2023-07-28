5eafb02047757cdc1ee4c56add52a8fe0059fcd5
hannesill <hill@login-1.GPU.CIT-EC.NET>
2023-07-28 17:24:04 +0200

    New results

--- param.sh
+++ param.sh
@@ -8,13 +8,14 @@
 #SBATCH -p gpu
 #SBATCH -c 2
 #SBATCH --gres=gpu:1
-#SBATCH -w schlumpfine
+#SBATCH -w handy
 #SBATCH --time=100:00:00
 
 # Checkout right git branch
-git checkout main
+git checkout inverse_relations_in_train
+rm data/fb15k-237/processed.pt
 # Load the conda environment
 eval "$(~/miniconda3/bin/conda shell.bash hook)"
 conda activate LiteralE
 
-python3 main.py --lit_mode num --eta 5
+python3 main.py --lit_mode num
