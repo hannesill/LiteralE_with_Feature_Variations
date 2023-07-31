8e2290ab59714442b4583b71ae95e5dc9905d4a2
hannesill <hill@login-1.GPU.CIT-EC.NET>
2023-07-31 14:14:39 +0200

    Update run script

--- param.sh
+++ param.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --partition=study
 #SBATCH --job-name=linkpred
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH -p gpu
-#SBATCH -c 2
+#SBATCH -w servant-2
 #SBATCH --gres=gpu:1
-#SBATCH -w papaschlumpf
-#SBATCH --time=100:00:00
+#SBATCH --time=40:00:00
 
+# Checkout right git branch
+rm data/fb15k-237/processed.pt
 # Load the conda environment
 eval "$(~/miniconda3/bin/conda shell.bash hook)"
 conda activate LiteralE
 
-python3 main.py --lit_mode none  --epochs 4000
+python3 main.py --lit_mode num --eta 5
