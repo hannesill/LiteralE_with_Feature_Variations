23d05cb7404e6bf1655c8441e500c6c29a933714
hannesill <hill@login-1.GPU.CIT-EC.NET>
2023-07-31 14:06:35 +0200

    Correct run script

--- param.sh
+++ param.sh
@@ -6,13 +6,11 @@
 #SBATCH --job-name=linkpred
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH -p gpu
 #SBATCH -c 2
 #SBATCH --gres=gpu:1
-#SBATCH --time=100:00:00
+#SBATCH --time=40:00:00
 
 # Checkout right git branch
-git checkout inverse_relations_in_train
 rm data/fb15k-237/processed.pt
 # Load the conda environment
 eval "$(~/miniconda3/bin/conda shell.bash hook)"
