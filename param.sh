252b6f39c747215559ce81715285638e3b705f15
hannesill <hill@login-1.GPU.CIT-EC.NET>
2023-08-02 08:26:30 +0200

    New results

--- param.sh
+++ param.sh
@@ -4,14 +4,13 @@
 #SBATCH --job-name=linkpred
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH -w servant-2
 #SBATCH --gres=gpu:1
 #SBATCH --time=40:00:00
 
 # Checkout right git branch
-rm data/fb15k-237/processed.pt
+
 # Load the conda environment
 eval "$(~/miniconda3/bin/conda shell.bash hook)"
 conda activate LiteralE
 
-python3 main.py --lit_mode num --eta 5
+python3 main.py --eta 5
