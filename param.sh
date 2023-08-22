b29968bf45e1ef32eadc62a90b47ea7a56a480e4
hannesill <hill@login-1.GPU.CIT-EC.NET>
2023-08-22 22:17:18 +0200

    New results

--- param.sh
+++ param.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
 #SBATCH --gres=gpu:1
-#SBATCH --time=50:00:00
+#SBATCH --time=120:00:00
 
 # Checkout right git branch
 
@@ -13,4 +13,4 @@
 eval "$(~/miniconda3/bin/conda shell.bash hook)"
 conda activate LiteralE
 
-python3 main.py --lit_mode txt --cluster 100
+python3 main.py --lit_mode all --scoring ConvE --epochs 300 --eta 50 --emb_dim 100
