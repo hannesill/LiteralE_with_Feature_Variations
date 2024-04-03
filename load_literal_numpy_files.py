e65aed5542c132131fa161c16ee43348fc525d90
hill <hannes.ill@tum.de>
2024-04-03 17:31:47 +0200

    Add scripts for paper on literal evaluation

--- load_literal_numpy_files.py
+++ load_literal_numpy_files.py
@@ -20,10 +20,15 @@ if __name__ == "__main__":
     literals_num_v1 = np.load("data/FB15k-237/numerical_literals_rep_attr.npy", allow_pickle=True)
     print(literals_num_v1.shape)
 
+    # Load variation 1 numerical literal array from file
+    literals_num_v1 = np.load("data/FB15k-237/numerical_literals_rep_attr_mean.npy", allow_pickle=True)
+    print(literals_num_v1.shape)
+
     # Load variation 2 numerical literal array from file
     literals_num_v2 = np.load("data/FB15k-237/numerical_literals_rep_filtered_100.npy", allow_pickle=True)
     print(literals_num_v2.shape)
 
     # Load variation 3 text literal array from file
-    literals_text_v3 = np.load("data/FB15k-237/text_literals_rep_clustered_100.npy", allow_pickle=True)
+    literals_text_v3 = np.load("data/FB15k-237/text_literals_rep_clustered_100_mean.npy", allow_pickle=True)
     print(literals_text_v3.shape)
+    print(literals_text_v3[0:5])
